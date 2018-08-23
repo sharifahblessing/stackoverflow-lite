61b363c82820e8d2c294ab454a543c46191cd855
sharifahblessing <snaluswata18@gmail.com>
2018-08-23 21:35:06 +0300

    add apiary for doc

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
+from flask_restful import Resource, Api
 from app import app
-
-
-
 
 @app.route('/',methods=['GET'])
 def index():
-    return('welcome to stackoverflow lite')
-
-if __name__=="__main__":
-    app.run(port=5000,debug=True)
+    return redirect("https://tuvugeapi.docs.apiary.io/#",code=302)
+if __name__ == '__main__':
+    app.run(port=5001,debug=True)
