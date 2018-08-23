12600dbe3a5026fe2de5a8596529ab657c149692
sharifahblessing <snaluswata18@gmail.com>
2018-08-23 21:34:02 +0300

    add

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
-from flask import Flask, redirect
-from flask_restful import Resource, Api
+from flask import Flask
 from app import app
+
+
+
 
 @app.route('/',methods=['GET'])
 def index():
-    return redirect("https://stackoverflowlite6.docs.apiary.io/#",code=302)
-if __name__ == '__main__':
+    return('welcome to stackoverflow lite')
+
+if __name__=="__main__":
     app.run(port=5000,debug=True)
